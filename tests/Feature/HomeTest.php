b2708bfcfcb588ccfbcf94913dec9180254f5182
tky <takayamac@icloud.com>
2022-08-09 13:59:14 +0900

    home追加

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testBody()
+    {
+        $response = $this->get('/home');
+
+        $response->assertStatus(200);
+    }
+}
