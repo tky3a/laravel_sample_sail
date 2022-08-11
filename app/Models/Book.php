e1c31fd9de9bd8235cdb4121d2f431836ad1c7cf
tky <takayamac@icloud.com>
2022-08-11 20:51:08 +0900

    bookとbookdetailの紐付けを設定

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function detail()
+    {
+        return $this->hasOne('\App\Models\Bookdetail');
+    }
+}
