437096ecf8188325c02fb7c55884cadde86f689d
tky <takayamac@icloud.com>
2022-08-14 16:30:17 +0900

    laravel breeze 導入

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,56 @@
-<html lang="en">
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>login</title>
-</head>
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-<body>
-  <p>login</p>
-  @isset($errors)
-    <p style="color: red;">{{ $errors->first('message') }}</p>
-  @endisset
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-  <form action="{{ route('authenticate') }}" name="loginform" method="post">
-    @csrf
-    <dl>
-      <dt>メールアドレス:</dt>
-      <dd><input type="text" name="email" size="30" value="{{ old('email') }}"></dd>
-      <dt>パスワード:</dt>
-      <dd><input type="password" name="password" size="30"></dd>
-      <button type="submit" name="action" value="send">ログイン</button>
-    </dl>
-  </form>
-</body>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-</html>
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('login', [\App\Http\Controllers\LoginController::class, 'index'])
-    ->middleware('guest')->name('login');
-Route::post('login', [\App\Http\Controllers\LoginController::class, 'authenticate'])
-    ->middleware('guest')->name('authenticate');
-Route::get('logout', [App\Http\Controllers\LoginController::class, 'logout'])
-    ->middleware('auth')->name('logout');
-Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'create'])
-    ->middleware('guest')->name('register.create');
-Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'store'])
-    ->middleware('guest')->name('register.store');
-Route::get('/payload', App\Http\Controllers\ArticlePayloadAction::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+
+// Route::get('login', [\App\Http\Controllers\LoginController::class, 'index'])
+//     ->middleware('guest')->name('login');
+// Route::post('login', [\App\Http\Controllers\LoginController::class, 'authenticate'])
+//     ->middleware('guest')->name('authenticate');
+// Route::get('logout', [App\Http\Controllers\LoginController::class, 'logout'])
+//     ->middleware('auth')->name('logout');
+// Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'create'])
+//     ->middleware('guest')->name('register.create');
+// Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'store'])
+//     ->middleware('guest')->name('register.store');
+// Route::get('/payload', App\Http\Controllers\ArticlePayloadAction::class);
